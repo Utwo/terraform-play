f530d428a1c06f2c5cacea1092d0164f445fb9e3
Utwo <mihai.legat@gmail.com>
2022-07-05 18:52:11 +0300

    Change google_vpc_access_connector name

--- modules/web_app/network.tf
+++ modules/web_app/network.tf
@@ -1,5 +1,5 @@
 resource "google_vpc_access_connector" "cloudrun_vpc_serverless" {
-  name          = "cloudrun_vpc_serverless"
+  name          = "cloudrun-vpc-serverless"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
 }
